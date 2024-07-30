f49684a734b2e02eb55782bc72af229195bf0c38
Amit Kumar <amit4.jammu@gmail.com>
2024-07-30 23:01:16 +0530

    first

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>CloudAdmin</span>
+      <span>© Cloud Admin Dashboard </span>
+    </div>
+  );
+};
+
+export default Footer;
